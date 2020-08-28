32fb4d19915943c03ec119593df439efe516870b
Mike <Walker>
2020-08-28 19:39:39 +0100

    Collect all the Data.

---
+++ src/Area/Area.tsx
@@ -0,0 +1,26 @@
+﻿import React, {FunctionComponent, useEffect, useState} from "react";
+import {AreaData, fetchDataForArea} from "../services/coronaDataFetcher";
+
+interface AreaProps {
+    areaName: string;
+}
+
+export const Area: FunctionComponent<AreaProps> = ({areaName}) => {
+    const [data, setData] = useState<AreaData[]>([]);
+    
+    useEffect(() => {
+        fetchDataForArea(areaName).then(areaData => setData(areaData));
+    }, [areaName]);
+    
+    if (data.length === 0) {
+        return <div>Loading</div>
+    }
+    return (
+        <div>
+            <h2>{areaName}</h2>
+            <ol>
+                {data.map(day => <li>Date: {day.date}, Total Cases: {day.cumCasesBySpecimenDate}, New Cases: {day.newCasesBySpecimenDate}</li>)}
+            </ol>
+        </div>
+    );
+};
---
+++ src/services/coronaDataFetcher.ts
@@ -0,0 +1,16 @@
+﻿export interface AreaData {
+    date: string;
+    newCasesBySpecimenDate: number;
+    cumCasesBySpecimenDate: number;
+}
+
+const getUrl = (areaName: string) => {
+    return `https://api.coronavirus-staging.data.gov.uk/v1/data?filters=areaName=${areaName}&structure=%7B%22date%22:%22date%22,%22areaName%22:%22areaName%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D`;
+};
+
+export const fetchDataForArea = async (areaName: string): Promise<AreaData[]> => {
+    const response = await fetch(getUrl(areaName));
+    const json = await response.json();
+    console.log(json.length, areaName);
+    return json.data;
+};
