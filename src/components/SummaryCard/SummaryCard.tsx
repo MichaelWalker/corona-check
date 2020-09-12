5b13fa83203f2b5e10a34155f62e4f2397cdc7cd
Mike <Walker>
2020-09-12 02:56:54 +0100

    Card templates added

---
+++ src/components/SummaryCard/SummaryCard.tsx
@@ -0,0 +1,19 @@
+﻿import React, {FunctionComponent} from "react";
+import styles from "./SummaryCard.module.scss";
+
+interface SummaryCardProps {
+    className?: string | undefined;
+    areaName: string;
+}
+
+export const SummaryCard: FunctionComponent<SummaryCardProps> = ({className, areaName}) => {
+    return (
+        <section className={`${styles.card} ${className || ""}`}>
+            <h2 className={styles.title}>{areaName}</h2>
+        </section>
+    );  
+};
+
+export const LargeSummaryCard: FunctionComponent<SummaryCardProps> = ({areaName}) => {
+    return <SummaryCard areaName={areaName} className={styles.largeCard}/>
+}
--- src/components/SummaryPage/SummaryPage.tsx
+++ src/components/SummaryPage/SummaryPage.tsx
@@ -1,5 +1,6 @@
 ﻿import React, {FunctionComponent} from "react";
 import styles from "./SummaryPage.module.scss";
+import {LargeSummaryCard, SummaryCard} from "../SummaryCard/SummaryCard";
 
 interface SummaryPageProps {
     title: string;
@@ -8,8 +9,13 @@ interface SummaryPageProps {
 export const SummaryPage: FunctionComponent<SummaryPageProps> = ({title}) => {
     return (
         <main className={styles.page}>
-            <h1>{title}</h1>
-            <p>Coming Soon!</p>
+            <div className={styles.cardContainer}>
+                <LargeSummaryCard areaName={"United Kingdom"}/>
+                <SummaryCard areaName={"England"}/>
+                <SummaryCard areaName={"Scotland"}/>
+                <SummaryCard areaName={"Wales"}/>
+                <SummaryCard areaName={"Northern Ireland"}/>
+            </div>
         </main>
     );  
 };
