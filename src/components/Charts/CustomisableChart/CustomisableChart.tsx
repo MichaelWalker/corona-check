78295d8a4aea0cab4f2c1fabfd33fe8a9119cf8d
Mike <Walker>
2020-09-19 23:59:24 +0100

    Add Brush

--- src/components/Charts/CustomisableChart/CustomisableChart.tsx
+++ src/components/Charts/CustomisableChart/CustomisableChart.tsx
@@ -1,6 +1,6 @@
 ﻿import React, {FunctionComponent} from "react";
 import styles from "./CustomisableChart.module.scss";
-import {Bar, ComposedChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {Bar, Brush, ComposedChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
 import {DataPoint, TimeSeries} from "../../../services/dataStructures";
 import moment from "moment";
 
@@ -20,16 +20,21 @@ export const CustomisableChart: FunctionComponent<CustomisableChartProps> = ({da
                     <Bar dataKey={dataKey} fill={"#413ea0"}/>
                     <XAxis dataKey={"timestamp"} 
                            type={"number"} 
-                           domain={['dataMin', 'dataMax']}
+                           domain={['dataMin - 36000', 'dataMax + 36000']}
                            scale={"utc"}
                            tickFormatter={timestamp => moment.unix(timestamp).format("DD-MMM")}
                            interval={"preserveEnd"}
                     />
                     <YAxis width={50} 
                            scale={scale} 
-                           domain={[1, "auto"]} 
+                           domain={["dataMin", "auto"]} 
                            allowDataOverflow={true} 
                            allowDecimals={true}
+                    />
+                    <Brush dataKey='timestamp' 
+                           height={30} 
+                           stroke="#8884d8" 
+                           tickFormatter={timestamp => moment.unix(timestamp).format("DD-MMM")}
                     />
                 </ComposedChart>
             </ResponsiveContainer>
